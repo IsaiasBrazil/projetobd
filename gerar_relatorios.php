d67b135dd8904ef99e510453e77acf1b5ce7f500
feli0x1 <sdiylaogzbhk@proton.me>
2023-04-27 16:24:47 -0300

    Parte de gerar relatório em PDF dos produtos implementada

--- gerar_relatorios.php
+++ gerar_relatorios.php
@@ -1,2 +1,53 @@
 <?php
+    require('fpdf185/fpdf.php');
+    $tabelas = array('produto');
+
+    function gerar_relatorios($lista_tabelas) {
+        $qt_resultados_por_pagina = 40;
+        $nome_arq_relatorio;
+        require('conexao.php');
+
+        foreach ($lista_tabelas as $tabela) {
+            $pdf = new FPDF('L', 'mm', 'A4');
+            $pdf->AddPage();
+            $pdf->SetFont('Arial', 'B', 14);
+
+            if ($tabela == 'produto') {
+                $nome_arq_relatorio = 'relatorio_produtos.pdf';
+                $query = 'SELECT * FROM produto';
+                $resu = mysqli_query($con, $query);
+                $qt_registros = mysqli_num_rows($resu);
+
+                // Criando os nomes das colunas
+                $pdf->Cell(15, 6, 'cod', 1, 0, 'C');
+                $pdf->Cell(110, 6, 'nome', 1, 0, 'C');
+                $pdf->Cell(40, 6, 'preco', 1, 0, 'C');
+                $pdf->Cell(35, 6, 'qtd_estoque', 1, 0, 'C');
+                $pdf->Cell(42, 6, 'unidade_medida', 1, 0, 'C');
+                $pdf->Cell(42, 6, 'fk_categoria_id', 1, 0, 'C');
+                $pdf->Ln();
+
+                $i = 0;
+                while ($linha = mysqli_fetch_assoc($resu)) {
+                    // if ($i % $qt_resultados_por_pagina == 0 && $i != 0) {
+                    //     $pdf->AddPage();
+                    // }
+
+                    $pdf->Cell(15, 6, $linha['cod'], 1, 0, 'C');
+                    $pdf->Cell(110, 6, $linha['nome'], 1, 0, 'C');
+                    $pdf->Cell(40, 6, $linha['preco'], 1, 0, 'C');
+                    $pdf->Cell(35, 6, $linha['qtd_estoque'], 1, 0, 'C');
+                    $pdf->Cell(42, 6, $linha['unidade_medida'], 1, 0, 'C');
+                    $pdf->Cell(42, 6, $linha['fk_categoria_id'], 1, 0, 'C');
+                    $pdf->Ln();
+
+                    $i++;
+                }
+            }
+        } 
+
+        $pdf->Output($nome_arq_relatorio, 'F');
+    }
+
+    gerar_relatorios($tabelas);
 ?>
