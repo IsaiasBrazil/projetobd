ae51fb346b88b69ef0f539383212c5c628099a06
IsaiasBrazil <isaias.brazil@hotmail.com>
2023-04-27 21:43:52 -0300

    acertada a estrutura da classe

--- Model/venda.php
+++ Model/venda.php
@@ -4,18 +4,16 @@
     private $cod;
     private $cod_cliente;
     private $cod_vendedor;
-    private $venda;
     private $data;
     private $prazo_entrega;
     private $cond_pagto;
     private $itens_vendas;
 
-    public function __construct($cod, $cliente, $vendedor, $venda, $data, $prazo_entrega, $cond_pagto, $itens_vendas)
+    public function __construct($cod, $cliente, $vendedor, $data, $prazo_entrega, $cond_pagto, $itens_vendas)
     {
         $this->cod = $cod;
         $this->cod_cliente = $cliente;
         $this->cod_vendedor = $vendedor;
-        $this->venda = $venda;
         $this->data = $data;
         $this->prazo_entrega = $prazo_entrega;
         $this->cond_pagto = $cond_pagto;
