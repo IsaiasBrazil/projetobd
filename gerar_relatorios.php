612c2b6acfe62c4098b549d954d2069cf041383b
feli0x1 <sdiylaogzbhk@proton.me>
2023-05-02 20:21:07 -0300

    categoria do relatório de produto arrumada

--- gerar_relatorios.php
+++ gerar_relatorios.php
@@ -13,7 +13,7 @@
 
             if ($tabela == 'produto') {
                 $nome_arq_relatorio = 'relatorio_produtos.pdf';
-                $query = 'SELECT * FROM produto';
+                $query = 'SELECT p.cod, p.nome, p.preco, p.qtd_estoque, p.unidade_medida, c.descricao FROM produto p INNER JOIN categoria c WHERE p.fk_categoria_id = c.cod GROUP BY p.cod';
                 $resu = mysqli_query($con, $query);
                 $qt_registros = mysqli_num_rows($resu);
 
@@ -23,17 +23,20 @@
                 $pdf->Cell(40, 6, 'preco', 1, 0, 'C');
                 $pdf->Cell(35, 6, 'qtd_estoque', 1, 0, 'C');
                 $pdf->Cell(42, 6, 'unidade_medida', 1, 0, 'C');
-                $pdf->Cell(42, 6, 'fk_categoria_id', 1, 0, 'C');
+                $pdf->Cell(42, 6, 'categoria', 1, 0, 'C');
                 $pdf->Ln();
 
                 $i = 0;
                 while ($linha = mysqli_fetch_assoc($resu)) {
+                    $nome = str_replace('”', '"', $linha['nome']);
+                    $nome = utf8_decode($nome);
+                    $descricao = utf8_decode($linha['descricao']);
                     $pdf->Cell(15, 6, $linha['cod'], 1, 0, 'C');
-                    $pdf->Cell(110, 6, $linha['nome'], 1, 0, 'C');
+                    $pdf->Cell(110, 6, $nome, 1, 0, 'C');
                     $pdf->Cell(40, 6, $linha['preco'], 1, 0, 'C');
                     $pdf->Cell(35, 6, $linha['qtd_estoque'], 1, 0, 'C');
                     $pdf->Cell(42, 6, $linha['unidade_medida'], 1, 0, 'C');
-                    $pdf->Cell(42, 6, $linha['fk_categoria_id'], 1, 0, 'C');
+                    $pdf->Cell(42, 6, $descricao, 1, 0, 'C');
                     $pdf->Ln();
 
                     $i++;
