501161648747fd4a8e0a3fe4fd242c18bc4742d9
feli0x1 <sdiylaogzbhk@proton.me>
2023-05-15 16:10:45 -0300

    Função para forçar o usuário a inserir um limite com valor >= 0 implementada

--- cad_alt_cliente.php
+++ cad_alt_cliente.php
@@ -44,19 +44,29 @@ if (isset($_GET['cod'])) {
     <script>
         function mascara_telefone(elem) {
             if (!elem.value) return elem.value = "";
-            elem.value = elem.value.replace(/\D/g,'')
-            elem.value = elem.value.replace(/(\d{2})(\d)/,"($1)$2")
-            elem.value = elem.value.replace(/(\d)(\d{4})$/,"$1-$2")
+            elem.value = elem.value.replace(/\D/g, '')
+            elem.value = elem.value.replace(/(\d{2})(\d)/, "($1)$2")
+            elem.value = elem.value.replace(/(\d)(\d{4})$/, "$1-$2")
         }
         
         function mascara_cpf(elem) {
             if (!elem.value) return elem.value = "";
-            elem.value = elem.value.replace(/\D/g,'')
+            elem.value = elem.value.replace(/\D/g, '')
             elem.value = elem.value.replace(/(\d{3})(\d)/, "$1.$2")
             elem.value = elem.value.replace(/(\d{3})(\d)/, "$1.$2")
             elem.value = elem.value.replace(/(\d{3})(\d{1,2})$/, "$1-$2")
-           }   
+        } 
+
+        function mascara_numeros_positivos(elem) {
+            elem.value = elem.value.replace(/[^\d.]/g, '');
+
+            var numero = parseFloat(elem.value);
+            if (isNaN(numero) || numero < 0) {
+                elem.value = '';
+            }
+        }
     </script>
+
 </head>
 
 <body>
@@ -87,7 +97,7 @@ if (isset($_GET['cod'])) {
                         <P> CPF:<input value='<?=$cpf?>' style="border-width: 3px; margin-left: 32px;" type="text" name="cpf" size="14" minlength="14" maxlength="14" placeholder="xxx.xxx.xxx-xx" onkeyup="mascara_cpf(this)" required></P>
                         <P> Telefone:<input value='<?=$telefone?>' onkeyup="mascara_telefone(this)" style="border-width: 3px;margin-left: 5px;" type="tel" name="telefone" size="14" minlength="14" maxlength="14" placeholder="(xx)xxxxx-xxxx" required></P>
                         <P> Email:<input value='<?=$email?>' style="border-width: 3px;margin-left: 23px;" type="email" name="email" size="14" maxlength="100" required></P>
-                        <P> Limite:<input value='<?=$limite?>' style="border-width: 3px;margin-left: 19px;" type="text" placeholder="R$ 0.00" name="limite" size="14" required></P>
+                        <P> Limite:<input value='<?=$limite?>' style="border-width: 3px;margin-left: 19px;" type="text" placeholder="R$ 0.00" name="limite" size="14" oninput="mascara_numeros_positivos(this)" required></P>
                         Estado:
                         <select style="border-width: 3px;margin-left: 14px;" name="estado" id="estado">
                             <?php
@@ -99,7 +109,7 @@ if (isset($_GET['cod'])) {
                             }
                             ?>
                         </select>
-                        <p> Cidade<input value='<?=$cidade?>' style="border-width: 3px;margin-left: 21px;" type="text" size="80"
+                        <p> Cidade:<input value='<?=$cidade?>' style="border-width: 3px;margin-left: 21px;" type="text" size="80"
                                 maxlength="80" name="cidade" required></p>
                         <p> Endereço:<input value='<?=$endereco?>' style="border-width: 3px;margin-left: 2px;" type="text" size="80"
                                 maxlength="100" name="endereco" required></p>
