921cb310a23147822ef2ab227f938af24b4bac01
IsaiasBrazil <isaias.brazil@hotmail.com>
2023-05-19 19:13:05 -0300

    pequenas correções em lista_cliente

--- lista/lista_cliente.php
+++ lista/lista_cliente.php
@@ -1,19 +1,34 @@
-<?php
-if (session_status() !== PHP_SESSION_ACTIVE) {
-    session_start();
-}
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <?php
+    if (session_status() !== PHP_SESSION_ACTIVE) {
+        session_start();
+    }
 
 
 
-include_once("../conexao.php");
-$query = "SELECT * FROM cliente";
-$result = mysqli_query($con, $query);
-include_once("../gridgenerico.php");
-grid($result, "CLIENTES");
+    include_once("../conexao.php");
+    $query = "SELECT * FROM cliente";
+    $result = mysqli_query($con, $query);
+    include_once("../gridgenerico.php");
+    ?>
+</head>
+
+<body>
+    <?php
+    grid($result, "CLIENTES");
 
 
-if (isset($_SESSION['msg'])) {
-    $msg = $_SESSION['msg'];
-    echo $msg;
-    unset($_SESSION['msg']);
-}
+    if (isset($_SESSION['msg'])) {
+        $msg = $_SESSION['msg'];
+        echo $msg;
+        unset($_SESSION['msg']);
+    }
+
+    ?>
+</body>
+
+</html>
