fd262310a7706046154692afaf0a475576ffe626
IsaiasBrazil <isaias.brazil@hotmail.com>
2023-05-20 15:43:52 -0300

    filtro por data nas vendas

--- lista/lista_venda.php
+++ lista/lista_venda.php
@@ -3,22 +3,62 @@
 <html>
 
 <head>
-<?php
-if (session_status() !== PHP_SESSION_ACTIVE) {
-    session_start();
-}
-include_once("../conexao.php");
-$query = "SELECT * FROM venda";
-$result = mysqli_query($con, $query);
-?>
-</head>
-<body>
-<div style="overflow:auto">
     <?php
+    if (session_status() !== PHP_SESSION_ACTIVE) {
+        session_start();
+    }
+    include_once("../conexao.php");
     require_once("../gridgenerico.php");
-    grid($result,"VENDAS");
+    $_SESSION['filtro_data_ini'] = $_POST['filtro_data_ini'] ??$_SESSION['filtro_data_ini']??"";//se post existir seta para o post, senão se o session existir seta para o session, senão vira ""
+    $_SESSION['filtro_data_fin'] = $_POST['filtro_data_fin'] ??$_SESSION['filtro_data_fin']??"";
     ?>
-</div>
-</body>
+    <style>
+        label {
+            padding-left: 20px;
+        }
+    </style>
+</head>
 
+<body>
+    <div>
+        <form method="POST">
+            <label style="font-weight: bold;">FILTRAR POR DATA:</label>
+            <label for="filtro_data_ini">Data inicial:</label>
+            <input type="date" name="filtro_data_ini" id="filtro_data_ini" value="<?=$_SESSION['filtro_data_ini'];?>">
+            <label for="filtro_data_fin">Data final:</label>
+            <input type="date" name="filtro_data_fin" id="filtro_data_fin" value="<?=$_SESSION['filtro_data_fin'];?>">
+            <input type="submit" value="Aplicar filtro">
+        </form>
+    </div>
+    <div style="overflow:auto">
+        <?php
+        $data_ini = $_POST['filtro_data_ini'] ?? "";
+        $data_fin = $_POST['filtro_data_fin'] ?? "";
+        $_SESSION['filtro_data_ini'] = $data_ini;
+        $_SESSION['filtro_data_fin'] = $data_fin;
+        if ($data_ini != "" && $data_fin != "") {
+            $query = "SELECT * FROM venda WHERE DATE(data)>='$data_ini' and data<='$data_fin'";
+        } else
+            $query = "SELECT * FROM venda";
+        $result = mysqli_query($con, $query);
+        grid($result, "VENDAS");
+        ?>
+    </div>
+    <script>
+        function atualizaData(e) {
+            if (e.value === "") {
+                elementoData = new Date;
+                elementoData.setHours(new Date().getHours() - 3);
+                if(e.name=="filtro_data_ini")
+                elementoData.setDate(new Date().getDate() - 30);
+                var hoje = elementoData.toISOString().split('T')[0];
+                e.value = hoje;
+            };
+        };
+        var data1 = document.getElementById('filtro_data_ini');
+        var data2 = document.getElementById('filtro_data_fin');
+        atualizaData(data1);
+        atualizaData(data2);
+    </script>
+</body>
 </html>
