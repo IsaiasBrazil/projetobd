db658ae46e526a4301e734684b441eddbbf6ffbf
IsaiasBrazil <isaias.brazil@hotmail.com>
2023-05-20 12:10:52 -0300

    filtro por nome no lista_cliente

--- lista/lista_cliente.php
+++ lista/lista_cliente.php
@@ -8,14 +8,23 @@
         session_start();
     }
     include_once("../conexao.php");
-    $query = "SELECT * FROM cliente";
-    $result = mysqli_query($con, $query);
     include_once("../gridgenerico.php");
     ?>
 </head>
+
 <body>
+    <div>
+        <form method="POST">
+            <label for="filtro">Filtrar por nome:</label>
+            <input type="text" name="filtro" id="filtro">
+            <input type="submit" value="Aplicar filtro">
+        </form>
+    </div>
     <div style="overflow:auto;">
         <?php
+        $nome = $_POST['filtro']??"";
+        $query = "SELECT * FROM cliente c where c.nome like '%$nome%'";
+        $result = mysqli_query($con, $query);
         grid($result, "CLIENTES");
         if (isset($_SESSION['msg'])) {
             $msg = $_SESSION['msg'];
